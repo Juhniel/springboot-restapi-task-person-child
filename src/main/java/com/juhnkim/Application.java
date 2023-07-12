d935a3a13965c33ed40aa48bc0e051b14dcdef63
Juhniel <juhn.sweden@gmail.com>
2023-07-12 13:30:50 +0200

    lambda

--- src/main/java/com/juhnkim/Application.java
+++ src/main/java/com/juhnkim/Application.java
@@ -1,7 +1,16 @@
 package com.juhnkim;
 
+import com.juhnkim.entity.Person;
+import com.juhnkim.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +19,25 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    private PersonService personService;
+
+    public Application(PersonService personService) {
+        this.personService = personService;
+    }
+
+
+    @Bean
+    public Supplier<List<Person>> findAll() {
+        return () -> personService.findAll();
+    }
+
+    @Bean
+    public Function<Person, Person> save() {
+        return input -> personService.save(input);
+    }
+
+    @Bean
+    public Supplier<Optional<Person>> findOldestChild() {
+        return () -> personService.findPersonWithOldestChild();
+    }
 }
